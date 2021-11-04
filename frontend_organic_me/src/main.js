404422fab25849f6441abea8f7327d197c2d860c
Adrian Nykiel <adrian@DESKTOP-E5OD8FS.localdomain>
2021-11-04 10:17:01 +0000

    initial commit

---
+++ frontend_organic_me/src/main.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import { createApp } from 'vue'
+import App from './App.vue'
+// import router from './router'
+// import store from './store'
+
+//Vue.config.productionTip = false
+createApp(App).mount('#app')
